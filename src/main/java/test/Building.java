8b9c1f42473808743676101476ba521f5fde4d63
Diana_Aimbetova <diana_aimbetova@epam.com>
2017-12-11 12:32:07 +0600

    исправлены некоторые методы

--- src/main/java/test/Building.java
+++ src/main/java/test/Building.java
@@ -38,97 +38,30 @@ public class Building{
 	        this.rooms = rooms;
 	    }
 
+    public Room getRoom(String roomName){
+        List<Room> rooms = getRooms();
+        //	System.out.println("------" + rooms.size());
+        Room myRoom = null;
+        for (Room room : rooms) {
+            if(room.getRoomName()== roomName){
+                myRoom = room;
+                return myRoom;
 
-	    public void describe() throws IlluminanceTooMuchException, SpaceUsageTooMuchException {
+            }
+        }
+
+        return myRoom;
+    }
+
+
+
+	    public void describe() {
 
 				System.out.println(getName());
 
-				List<Room> rooms = getRooms();
-				for (Room room : rooms) {
-				    try{
-					System.out.println(room.getRoomName());
-                    }catch (NullPointerException  n) {
-                        System.out.println("Комната не может быть null");
-                    }
-                    try {
-                        List<Lamp> lamps = room.getLamps();
+                for(Room room : rooms){
 
-                            for (Lamp lamp : lamps) {
-                                if (lamp.getLampLight() > 0) {
-                                    //System.out.println("----------  " + lamp.getLampLight());
-                                    if (lamps.size() > 1) {
-                                        totalLight += lamp.getLampLight();
-                                    } else {
-                                        totalLight = lamp.getLampLight();
-                                    }
-
-                                } else {
-                                    throw new IllegalArgumentException("Освещение ламп не может быть отрицательным числом");
-                                }
-                            }
-
-                    } catch (NullPointerException  n) {
-                        System.out.println("Лампочки не могут быть null");
-                    }
-                    try{
-				        if(room.getWindowNum() > 0) {
-
-                            if ((room.getWindowNum() * 700 + totalLight) < MAXLIGHT) {
-                                System.out.println("Освещённость = " + (room.getWindowNum() * 700 + totalLight) + " (" + room.getWindowNum() + " окна по 700 лк, лампочки " + totalLight + "лк)");
-                            } else {
-                                throw new IlluminanceTooMuchException("Освещение превышает максимум!");
-                            }
-                        }else{
-                           throw new IllegalArgumentException("Количество окон не может быть отрицательным числом");
-                        }
-                }catch (NullPointerException  n) {
-                    System.out.println("Комната is null, нельзя посчитать количество окон ");
-                }
-                    try{
-					List<Furniture> furnitures = room.getFurnitures();
-					if (furnitures.isEmpty()) {
-						System.out.println("Мебели нет");
-					} else {
-						System.out.println("Мебель:");
-
-						for (Furniture furniture : furnitures) {
-                            if (furniture.furnitureSquare() > 0) {
-                                System.out.println(furniture.furnitureName() + " (площадь= " + furniture.furnitureSquare() + "м^2)");
-                                totalSquare += furniture.furnitureSquare();
-                            }else{
-                                throw new IllegalArgumentException("Площадь мебели не может быть отрицательным числом");
-                            }
-						}
-
-
-					}
-                    }catch (NullPointerException  n) {
-                        System.out.println("Мебель не может быть null");
-                    }
-                    try{
-				        if(room.getSquare() > 0) {
-                            int percentFree = 100 - ((totalSquare * 100) / room.getSquare());
-                            int percentFull = 100 - percentFree;
-                            if (percentFull < MAXSQUARE) {
-                                if (percentFree != 100) {
-                                    System.out.println("Площадь = " + room.getSquare() + "м^2 (занято " + totalSquare + "м^2)" + ", гарантированно свободно " + (room.getSquare() - totalSquare) + "м^2 или " + percentFree + "% площади");
-                                } else {
-                                    System.out.println("Площадь = " + room.getSquare() + "м^2 (свободно 100%) ");
-
-                                }
-                            } else {
-                                throw new SpaceUsageTooMuchException("Слишком мало свободного пространства!");
-                            }
-                        }else{
-                        throw  new IllegalArgumentException("Площадь комнаты не может быть отрицательным числом");
-                    }
-                    }catch (ArithmeticException ae) {
-                        System.out.println("Площадь комнаты не может быть равна 0");
-
-				    }catch (NullPointerException  n) {
-                System.out.println("Площадь комнаты равна 0, потому что комната is null");
-            }
-
+                    System.out.println(room.toString());
                 }
 
 
--- src/main/java/test/Room.java
+++ src/main/java/test/Room.java
@@ -7,7 +7,9 @@ public class Room {
 	 private int windowNum;
 	 private int square;
 	 private String roomName;
-	 
+	static final int MAXLIGHT = 4000;
+	static final int MAXSQUAREPERCENT = 70;
+	static final int WINDOWLIGHT = 700;
 	    private List<Furniture> furnitures = new ArrayList<>();
 	    private List<Lamp> lamps = new ArrayList<>();
 	    public Room(String roomName, int square, int windowNum) {
@@ -17,20 +19,32 @@ public class Room {
 	    }
 
 	    public void add(Furniture furniture) {
-	    	try {
-				furnitures.add(furniture);
-			}catch (NullPointerException  n){
-				System.out.println("index is out of range.");
-			}
+
+	    		if(isSpaceUsageTooMuch(furniture.furnitureSquare())==true){
+					//System.out.println("------" + isSpaceUsageTooMuch(furnitures));
+					furnitures.add(furniture);
+				} else {
+					try {
+						throw new SpaceUsageTooMuchException("Слишком мало свободного пространства!");
+					} catch (SpaceUsageTooMuchException e) {
+						e.printStackTrace();
+					}
+				}
 
 	    }
-	    
+
+
+
 	    public void add(Lamp lamp) {
-	    	try{
-	        lamps.add(lamp);
-		}catch (NullPointerException  n){
-		System.out.println("index is out of range.");
-	}
+	    	if(isIlluminanceTooMuch(lamp.getLampLight())==true) {
+				lamps.add(lamp);
+			} else{
+				try {
+					throw new IlluminanceTooMuchException("Освещение превышает максимум!");
+				} catch (IlluminanceTooMuchException e) {
+					e.printStackTrace();
+				}
+			}
 	    }
 	    
 	    public String getRoomName() {
@@ -72,4 +86,108 @@ public class Room {
 	    public void setLamps(List<Lamp> lamps) {
 	        this.lamps = lamps;
 	    }
+
+	    public int getTotalLightOfLamps(){
+			List<Lamp> lamps = getLamps();
+			int totalLight = 0;
+			for (Lamp l:lamps) {
+				totalLight+=l.getLampLight();
+			}
+
+			return totalLight;
+		}
+
+		public int getOccupiedSpace(){
+			List<Furniture> furnitures = getFurnitures();
+			int totalSquare=0;
+			for (Furniture f:furnitures) {
+				totalSquare+=f.furnitureSquare();
+				//System.out.println("---" + totalSquare);
+			}
+			//System.out.println("---" + totalSquare);
+			return totalSquare;
+
+		}
+
+		public int getFreeSpace(){
+	    	int notFree = getOccupiedSpace();
+	    	int freeSpace = 0;
+			int roomSquare = getSquare();
+			freeSpace = roomSquare - notFree;
+			return freeSpace;
+		}
+
+		public int freeSpacePercent() {
+			int notFree = getOccupiedSpace();
+			int freeSpace = getFreeSpace();
+			int roomSquare = getSquare();
+			int freeSpacePercent = 0;
+			freeSpacePercent = (100 - ((100*notFree)/roomSquare));
+			return freeSpacePercent;
+		}
+
+		public String  getFurnituresDesc(){
+			List<Furniture>  furnitures = getFurnitures();
+			if(!furnitures.isEmpty()) {
+				List<String> l = new ArrayList<>();
+				String name = "";
+				int sq = 0;
+				String stroka = "";
+				for (Furniture f : furnitures) {
+					name = f.furnitureName();
+					sq = f.furnitureSquare();
+					stroka = name + " (площадь " + sq + "м^2)";
+					//System.out.println("-----" + name);
+					l.add(stroka);
+
+				}
+				StringBuilder out = new StringBuilder();
+				for (Object o : l) {
+					out.append(o.toString());
+					out.append("\n");
+				}
+
+
+				//System.out.println("-----" + name);
+				return "Мебель:"  + "\n" + out.toString();
+			} else{
+				return "Мебели нет";
+			}
+		}
+
+	public boolean isSpaceUsageTooMuch(int square){
+		int totalSquare=getOccupiedSpace() + square;
+		int roomSquare = getSquare();
+		//System.out.println("room sqaure " + roomSquare);
+		//System.out.println("total square " + totalSquare);
+		int x = (100*totalSquare)/roomSquare;
+		//System.out.println("x = " + x);
+		if(x< MAXSQUAREPERCENT){
+			//System.out.println("***" + (x< MAXSQUAREPERCENT));
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isIlluminanceTooMuch(int light){
+		//lamps = getLamps();
+		int totalLight = getTotalLightOfLamps() + light;
+		//System.out.println("---" + totalLight);
+		int x = totalLight + (getWindowNum()*WINDOWLIGHT);
+		if(x < MAXLIGHT){
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public String toString(){
+	    	return getRoomName() + " " +
+					"\n" + "Освещённость = " + (getTotalLightOfLamps()+ (getWindowNum()*WINDOWLIGHT)) + "(" + getWindowNum() + " окна по 700 лк, лампочки "
+					+ getTotalLightOfLamps() + "лк)" +
+					"\n" + "Площадь = " + getSquare() + " м^2 (занято " + getOccupiedSpace() + " м^2, гарантированно свободно " +
+					+ getFreeSpace() + " м^2 или " + freeSpacePercent() + " % площади)" +
+					"\n" +  getFurnituresDesc()  ;
+	}
+
 }
